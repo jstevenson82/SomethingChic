8528386a9c595b23c8afe2fbc9bf4ec45e4d2252
Joseph Stevenson <joseph.stevenson82@gmail.com>
2011-04-29 13:04:02 -0400

    updated email to, updated layout gallery

--- app/mailers/request_information.rb
+++ app/mailers/request_information.rb
@@ -1,7 +1,7 @@
 class RequestInformation < ActionMailer::Base
    def request_information(request_info)  
    	 @request_info = request_info
-     mail(:to => "joseph.stevenson82@gmail.com", :subject => "Requesting info", :from => "request_information@SCPEdesign.com" )  
+     mail(:to => "joseph.stevenson82@gmail.com,chrichta@yahoo.com", :subject => "Requesting info", :from => "request_information@SCPEdesign.com" )  
    end 
    
    def request_information_ack(request_info)  
